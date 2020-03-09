f102bf5272f02c32fa5c437f4f48b60b2f5e6414
nik <niksama3@gmail.com>
2020-03-09 16:55:27 +0700

    2.0.1

--- app/src/main/java/com/nik/konverter/model/DatabaseRepository.kt
+++ app/src/main/java/com/nik/konverter/model/DatabaseRepository.kt
@@ -5,26 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.room.Room
 import com.nik.konverter.database.AppDatabase
 import com.nik.konverter.database.Valuta
+import com.nik.konverter.database.ValutaDao
 import com.nik.konverter.model.forms.ActionResult
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import org.simpleframework.xml.core.Validate
 
-class DatabaseRepository private constructor(val context: Context){
+class DatabaseRepository private constructor(private val valutaDao: ValutaDao) {
 
-
-
-    private val database: AppDatabase
-
-    init {
-        val DATABASE_NAME = "valuta_db"
-        database = Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).build()
+    companion object {
+        //Singleton instantiation
+        @Volatile var instance:DatabaseRepository? = null
+        fun getInstance(valutaDao: ValutaDao): DatabaseRepository {
+            return instance ?: synchronized(this) {
+                instance ?: DatabaseRepository(valutaDao).also { instance = it }
+            }
+        }
     }
 
-
-
     fun getValutas() = MutableLiveData<ActionResult>().apply {
-        database.valutaDao().getValuaList().subscribeOn(Schedulers.io())
+        valutaDao.getValuaList().subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread()).subscribe(object : DisposableSingleObserver<List<Valuta>>() {
                 override fun onSuccess(t: List<Valuta>) {
                     value = ActionResult.Success(t)
@@ -36,11 +37,11 @@ class DatabaseRepository private constructor(val context: Context){
             })
     }
 
-    fun deleteAll() = database.valutaDao().deleteAll().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    fun deleteAll() = valutaDao.deleteAll().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-    fun getById(id: String) = database.valutaDao().getValutaById(id).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    fun getById(id: String) = valutaDao.getValutaById(id).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-    fun insertValutas(valutas: List<Valuta>) = database.valutaDao().insert(valutas).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    fun insertValutas(valutas: List<Valuta>) = valutaDao.insert(valutas).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-    fun updateValutas(valutas: List<Valuta>) = database.valutaDao().update(valutas).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    fun updateValutas(valutas: List<Valuta>) = valutaDao.update(valutas).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 }
--- app/src/main/java/com/nik/konverter/model/SingletoneHolder.kt
+++ app/src/main/java/com/nik/konverter/model/SingletoneHolder.kt
@@ -3,10 +3,4 @@ package com.nik.konverter.model
 open class SingletoneHolder<out T: Any, in A>(creator: (A) -> T) {
     private var creator: ((A) -> T)? = creator
     @Volatile private var instance: T? = null
-
-    fun getInstance(arg: A): T {
-        return instance ?: synchronized(this) {
-            instance ?:
-        }
-    }
 }
