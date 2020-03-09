05a9fb9dbee77a45289c33fdaca5c71644f249c4
nik <niksama3@gmail.com>
2020-03-09 19:01:56 +0700

    replace Context on ValutaDao in repositories

--- app/src/main/java/com/nik/konverter/model/Repository.kt
+++ app/src/main/java/com/nik/konverter/model/Repository.kt
@@ -3,27 +3,30 @@ package com.nik.konverter.model
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import androidx.room.Database
 import com.nik.konverter.adapters.ValutaAdapter
 import com.nik.konverter.database.Valuta
+import com.nik.konverter.database.ValutaDao
 import com.nik.konverter.model.forms.ValCurs
 import com.nik.konverter.model.forms.ActionResult
 import com.nik.konverter.model.forms.Valute
 import io.reactivex.observers.DisposableCompletableObserver
 import io.reactivex.observers.DisposableSingleObserver
 
-class Repository {
+class Repository private constructor(private val valutaDao: ValutaDao) {
 
-    val remouteRepository = RemouteRepository()
+    private val remouteRepository = RemouteRepository()
+    private val databaseRepository = DatabaseRepository.getInstance(valutaDao)
 
-    fun updateData(context: Context) = MutableLiveData<ActionResult>().apply{
+    fun updateData() = MutableLiveData<ActionResult>().apply{
         remouteRepository.getValCurse().subscribe(object : DisposableSingleObserver<ValCurs>(){
 
             override fun onSuccess(valCurs: ValCurs) {
-                DatabaseRepository.getInstance(context).deleteAll().subscribe(object : DisposableCompletableObserver() {
+                DatabaseRepository.getInstance(valutaDao).deleteAll().subscribe(object : DisposableCompletableObserver() {
 
                     override fun onComplete() {
                         valCurs.valutes.add(0, Valute("R99999", "643", "RUB", "1", "Рубль", "1"))
-                        insertValutas(context, ValutaAdapter().convert(valCurs)).subscribe(object : DisposableCompletableObserver() {
+                        insertValutas(ValutaAdapter().convert(valCurs)).subscribe(object : DisposableCompletableObserver() {
 
                             override fun onComplete() {
                                 value = ActionResult.Success("Данные обновлены")
@@ -45,6 +48,6 @@ class Repository {
             }
         })
     }
-    fun insertValutas(context: Context, valutas: List<Valuta>) = DatabaseRepository.getInstance(context).insertValutas(valutas)
-    fun getValutasResult(context: Context) = DatabaseRepository.getInstance(context).getValutas()
+    fun insertValutas(valutas: List<Valuta>) = databaseRepository.insertValutas(valutas)
+    fun getValutasResult(valutaDao: ValutaDao) = databaseRepository.getValutas()
 }
